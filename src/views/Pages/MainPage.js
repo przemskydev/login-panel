7e44793ad7b56b03c5e1ccc5f3c8986e42cf9a12
przemskydev <przemekgithub@o2.pl>
2020-07-27 15:29:14 +0200

    adding theme folder with global style by styled-component plugin

--- src/views/Pages/MainPage.js
+++ src/views/Pages/MainPage.js
@@ -1,9 +1,11 @@
-import React from 'react'
-import Home from '../../components/Home'
+import React from 'react';
+import Home from '../../components/Home';
+import GlobalStyle from '../../theme/GlobalStyles'
 
 const MaginPage = () => {
   return (
     <div>
+      <GlobalStyle />
       <h1>Here will be Home page after correct Login</h1>
       <Home />
     </div>
