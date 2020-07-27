58c0b8900ad81b78fb57c8f275b0de5855222a2f
przemskydev <przemekgithub@o2.pl>
2020-07-27 17:37:59 +0200

    adding display grid to register component

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,14 +3,24 @@ import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
   border: 1px solid red;
-  min-height: 500px;
-  width: 400px
+  min-height: 70vh;
+  width: 60vw;
+  display: grid;
+  grid-template-columns: 200px auto;
+`;
+
+const StyledContainer = styled.div`
+  grid-column-start: 1;
+  grid-column-end: 2;
+  border-right: 1px solid yellow;
 `;
 
 const Register = () => {
   return (
     <StyledWrapper>
-      <h1>Hey</h1>
+      <StyledContainer>
+        Hey
+      </StyledContainer>
     </StyledWrapper>
   )
 }
--- src/views/Pages/RegisterPage.js
+++ src/views/Pages/RegisterPage.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Register from '../../components/Register'
+import styled from 'styled-components'
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 
 const RegisterPage = () => {
   return (
-    <div>
-      <h1>Here will be rejestraction Form</h1>
+    <StyledWrapper>
+      <h1>registration_form</h1>
       <Register />
-    </div>
+    </StyledWrapper>
   )
 }
 
