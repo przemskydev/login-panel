75475d27d412c7fb6fc0a3c16bdbe058050dd399
przemskydev <przemekgithub@o2.pl>
2020-07-27 15:30:37 +0200

    add global styles

---
+++ src/theme/GlobalStyles.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  
+  *, *::before, *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  html {
+    /* happy rems */
+    /* 1rem == 10px */
+    font-size: 62.5%; 
+  }
+  
+  body {
+    margin: 0;
+    padding: 0;
+    padding-left: 150px;
+    font-size: 1.6rem;
+    font-family: "Montserrat", sans-serif;
+  }
+
+  button:focus {
+    outline:0;
+  }
+
+  input:focus {
+    outline:0;
+  }
+`;
