e6b230721111dc54b33c638e88e3208b5d9f2424
przemskydev <przemekgithub@o2.pl>
2020-07-29 16:45:27 +0200

    WIP create action

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+export const addItem = (itemContent) => ({
+  type: "ADD",
+  payload: itemContent
+})
---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+  
+}
+
+const reducer = (state = initialState, {payload, type}) => {
+
+  if(type === "ADD"){
+    return {
+      ...state,
+      payload
+    }
+  }
+}
+
+export default reducer;
