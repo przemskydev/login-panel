6efe485fc8acbaf852f6e37b81b989ad65fc5ec9
przemskydev <przemekgithub@o2.pl>
2020-08-11 22:15:25 +0200

    Updates

---
+++ precache-manifest.a7a93ec0188f946288f19592aa2cd1ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43cf720ff550aa1428a8d8fdb6b6f114",
+    "url": "/login-panel/index.html"
+  },
+  {
+    "revision": "3f5b7b6a2a224f2383f0",
+    "url": "/login-panel/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "323dc97aab5cb698b17d",
+    "url": "/login-panel/static/js/2.7fcca597.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/login-panel/static/js/2.7fcca597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5b7b6a2a224f2383f0",
+    "url": "/login-panel/static/js/main.35ea89b2.chunk.js"
+  },
+  {
+    "revision": "915e8baf56adf8dde85f",
+    "url": "/login-panel/static/js/runtime-main.a4340978.js"
+  },
+  {
+    "revision": "37366ff7805de26beafef96c7a5b80c7",
+    "url": "/login-panel/static/media/exclamation-mark.37366ff7.svg"
+  },
+  {
+    "revision": "223b5d1f2eddf97e008b6ddfa5768617",
+    "url": "/login-panel/static/media/logout.223b5d1f.svg"
+  }
+]);
